a2b8f15fb0d857f1eaa92521dc765096c551619d
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-13 20:01:28 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,7 +43,7 @@ def naming_system
            label: "Family",
            sub_category: {
     label: "Genus",
-    sub_category: {label:"species",
+    sub_category: {label:"Species",
       sub_category: nil
     }}
          }
