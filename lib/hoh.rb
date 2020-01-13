f6f41e22bba7a402bf5590a6646d4fa3ac3cf024
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-13 19:49:46 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,13 @@
 # }
 
 def naming_system
-  Hoh = {}
+  Hoh = {
+#   label: "Kingdom",
+#   sub_category: {
+#     label: "Phylum",
+#     sub_category: {
+#       label: "Class"
+#     }
   # Remember:
   #  Kingdom
   #  Phylum
